36c50aebd914858869ed42df22d592cab036ffef
Bryan Marvila <bryan7c@gmail.com>
2016-06-02 19:56:03 -0300

    Commit página do projeto

--- public/script/filter/splitFilter.js
+++
@@ -1,8 +0,0 @@
-app.filter("split", function(){
-	return function(input, separator){
-		if(Object.prototype.toString.call( input ) === '[object Array]'){
-			var input = input.join(separator);
-		}
-		return input;
-	}
-})
